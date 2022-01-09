425bd136fbab0e2260f22dc4a8ccaf6f1dd6e1bf
clueless-skywatcher <somichat@gmail.com>
2022-01-09 11:32:53 +0530

    Fixed secret key

--- narro/settings.py
+++ narro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_q1)(2-na#o)dag-___4iqd0%5*7_!r$5^a$4c28$k+_cbok!v'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
